36b38f76ff0d853769ccaa807c9bf44c4a015bcc
Sami Horttanainen <sami1horttanainen@gmail.com>
2022-07-28 10:17:02 +0300

    Add reuseable react-hook-form components

---
+++ src/components/HookFormField.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Path, Control, RegisterOptions, useController } from "react-hook-form";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+
+type HookFormFieldProps<TFormValues> = TextFieldProps & {
+  control: Control<TFormValues>;
+  name: Path<TFormValues>;
+  rules?: RegisterOptions;
+}
+function HookFormField<TFormValues>({ control, name, rules, ...props }: HookFormFieldProps<TFormValues> ) {
+  const { field: { onChange, onBlur, name: fieldName, value },
+    fieldState: { error, isTouched } } = useController<TFormValues>({ control, name, rules });
+  const isError = !!(error && isTouched);
+  return(
+    <TextField
+      variant="standard"
+      sx={{ width: 220 }}
+      { ...props }
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+      label={fieldName}
+      error={isError}
+      helperText={isError && error.message}
+    />
+  );
+}
+
+export default HookFormField;
