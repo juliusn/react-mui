a1c83e8d6e6434a278a148f732a77747cf950b2f
Sami Horttanainen <sami1horttanainen@gmail.com>
2022-08-30 16:24:34 +0300

    fix keyboard input bug

--- src/components/HookFormTimePicker.tsx
+++ src/components/HookFormTimePicker.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { useController, Path, Control, RegisterOptions } from "react-hook-form";
+import { DesktopTimePicker } from "@mui/x-date-pickers/DesktopTimePicker";
+import { useController, Path, Control, PathValue, RegisterOptions } from "react-hook-form";
+import { parse } from "date-fns";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 
 type HookFormTimePickerProps<TFormValues> = TextFieldProps & {
@@ -13,25 +14,35 @@ type HookFormTimePickerProps<TFormValues> = TextFieldProps & {
 };
 
 export default function HookFormTimePicker<TFormValues>({ control, name, rules, ...props }: HookFormTimePickerProps<TFormValues>) {
-  const { field: { onChange, onBlur, value, name: fieldName },
+  const { field: { onChange, value, name: fieldName },
     fieldState: { error } } = useController<TFormValues>({ control, name, rules });
-
-
   return(
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <TimePicker
+      <DesktopTimePicker
         ampm={false}
+        openTo="hours"
+        views={["hours", "minutes"]}
+        inputFormat="HH:mm"
+        mask="__:__"
         value={value}
-        onChange={onChange}
+        onChange={(newValue, keyboardInput) => {
+          if(keyboardInput){
+            if(keyboardInput.length === 5){
+              onChange(parse(keyboardInput, "HH:mm", new Date()));
+            }
+          }
+          if(newValue && !keyboardInput){
+            onChange(newValue);
+          }
+        }}
         renderInput={(params) => (
           <TextField
             {...params}
             {...props}
             variant={"standard"}
             sx={{ width: 200 }}
-            onBlur={onBlur}
-            error={!!error}
             helperText={error && error.message}
+            error={!!(error)}
             label={props.label ? props.label : fieldName}
           />
         )}
